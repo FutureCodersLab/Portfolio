ebc925a5db22d82db7f42534d08c933d94dec7a0
Fernando Ordonez <fernando@nuli.app>
2025-03-09 23:43:12 +0800

    fix again images path

--- data-structures/links.js
+++ data-structures/links.js
@@ -1,18 +1,18 @@
 export const socialLinks = [
     {
         href: "https://www.github.com/",
-        icon: "/images/icons/socials/github-white.svg",
+        icon: "./images/icons/socials/github-white.svg",
     },
     {
         href: "https://www.linkedin.com/",
-        icon: "/images/icons/socials/linkedin.svg",
+        icon: "./images/icons/socials/linkedin.svg",
     },
     {
         href: "https://www.facebook.com/",
-        icon: "/images/icons/socials/facebook.svg",
+        icon: "./images/icons/socials/facebook.svg",
     },
     {
         href: "https://www.instagram.com/",
-        icon: "/images/icons/socials/instagram.svg",
+        icon: "./images/icons/socials/instagram.svg",
     },
 ];
