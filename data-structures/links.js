f68d82ffbf29325a264f458ed73e87f32748bdaf
Fernando Ordonez <fernando@nuli.app>
2025-03-09 23:33:27 +0800

    fix images path

--- data-structures/links.js
+++ data-structures/links.js
@@ -1,33 +1,18 @@
-export const homeLinks = [
-    {
-        href: "https://www.facebook.com/",
-        icon: '<i class="ri-facebook-circle-line"></i>',
-    },
-    {
-        href: "https://www.instagram.com/",
-        icon: '<i class="ri-instagram-line"></i>',
-    },
-    {
-        href: "https://www.github.com/",
-        icon: '<i class="ri-github-fill"></i>',
-    },
-];
-
 export const socialLinks = [
     {
         href: "https://www.github.com/",
-        icon: "../images/icons/socials/github-white.svg",
+        icon: "/images/icons/socials/github-white.svg",
     },
     {
         href: "https://www.linkedin.com/",
-        icon: "../images/icons/socials/linkedin.svg",
+        icon: "/images/icons/socials/linkedin.svg",
     },
     {
         href: "https://www.facebook.com/",
-        icon: "../images/icons/socials/facebook.svg",
+        icon: "/images/icons/socials/facebook.svg",
     },
     {
         href: "https://www.instagram.com/",
-        icon: "../images/icons/socials/instagram.svg",
+        icon: "/images/icons/socials/instagram.svg",
     },
 ];
